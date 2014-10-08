e3bad9938973bf6321d14ddaecd0ca0a4074525e
Tyler Chen <chentylerl@gmail.com>
2014-10-08 07:42:24 -0400

    minor updates

--- circles.py
+++ circles.py
@@ -1,29 +1,11 @@
 from PIL import Image, ImageDraw
-
-im = Image.open( "in.png" )
-width = im.size[0]
-height = im.size[1]
-
-scale = 5
-image = Image.new('RGBA', (width*scale, height*scale))
-draw = ImageDraw.Draw(image)
-
-def circle(x, y, r):
-  draw.ellipse((scale*(x-r), scale*(y-r), scale*(x+r), scale*(y+r) ), fill = 'black', outline ='black')
-
-#do u even use?
-def bw( c ):
-  sum = c[0] + c[1] + c[2]
-  if ( sum < 383 ):
-    return 1
-  else:
-    return 0
     
 def ave_color(xx, yy, n, img):
   r, g, b = 0, 0, 0
   count = 0
   for x in range(xx, xx+n):
     for y in range(yy, yy+n):
+      #print( x, y)
       count += 1
       p = img.getpixel( (x,y) )
       r+= p[0]
@@ -38,20 +20,32 @@ def ave_color(xx, yy, n, img):
   return c
   
 
-def scan(img, step):
-  block = int( width / step )
-  print( block )
-  for x in range(0, width, block):
-    for y in range(0, height, block):
-      print(x, y)
+def scan(img, step, scale):
+  w = img.size[0]
+  h = img.size[1]
+  block = int( w / step )
+  width = block * step
+  height = h // block * block
+  print( w, h, width, height )
+  img = img.crop((0, 0, width, height))
+  
+  image = Image.new('RGBA', (width*scale, height*scale))
+  draw = ImageDraw.Draw(image)
+  def circle(x, y, r):
+    draw.ellipse((scale*(x-r), scale*(y-r), scale*(x+r), scale*(y+r) ), fill = "black")
+
+  #better way to handle 
+  for x in range(0, width , block):
+    for y in range(0, height , block):
       darkness = 1 - ave_color(x, y, block, img)[3]/255
-      radius = block / 2 * darkness * 1.4
-      print( radius )
+      radius = block / 2 * darkness * 1.25
+      print(x, y, radius)
       
       circle( x+block/2, y+block/2, radius)
 
+  image.save('c.png')
 
-scan(im, 300)
-#ave_color(0,0,100,im)
-  
-image.save('out.png')
+im = Image.open( "c.jpg" )
+
+
+scan(im, 150, 5)
